e6c3770a1067e62fab7689a7e026727764340075
Rahmonbek <83601261+Rahmonbek@users.noreply.github.com>
2021-07-21 21:35:29 +0500

    sdfgh
    dfegrh

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Admin from './pages/Admin'
 
 export default class componentName extends Component {
   render() {
     return (
       <div>
         Hello Github
+        <Admin/>
       </div>
     )
   }
---
+++ src/pages/Admin.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Admin extends Component {
+    render() {
+        return (
+            <div>
+               How are you? 
+            </div>
+        )
+    }
+}
