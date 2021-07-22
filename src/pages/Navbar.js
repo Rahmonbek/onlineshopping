51308396c2c20daec3806f8da8d001049d1b29d9
Tursunov1818 <tursunov2229@gmail.com>
2021-07-22 22:29:20 +0500

    Update Navbar.js

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Navbar extends Component {
-    render() {
-        return (
-            <div>
-              <div className="navbar">  <span>Home</span>    <span> About</span>    <span> Contact</span></div>  
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div className="navbar">
+          <span>O`zb</span>
+          <span>Рус</span>
+          <span>Eng</span>
+        </div>
+      </div>
+    );
+  }
 }
