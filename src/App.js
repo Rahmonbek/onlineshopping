6537a5182344301545fa5b994364920bfbe60a69
Bahriddin0105 <87873365+Bahriddin0105@users.noreply.github.com>
2021-08-15 23:13:11 +0500

    1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Navbar from './pages/Navbar'
+import Companent from './pages/cards/Companent'
 import './App.css'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Dashboard from './pages/dashboard/Dashboard'
@@ -16,7 +17,9 @@ export default class componentName extends Component {
             </Route>
         </Switch>
        </BrowserRouter>
-      </div>
+       <Companent/>
+       
+      </div>  
     )
   }
 }
--- src/pages/cards/Companent.js
+++ src/pages/cards/Companent.js
@@ -1,12 +1,42 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import { Container } from "react-bootstrap";
+// import foto from '../images/foto.jpeg'
+import './Comp.css'
 export default class Companent extends Component {
-    
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  state = {
+    product: [
+      {
+        id: 1,
+        name: "Samsung Galaxy A32 4/64GB",
+        ichki_xotira: "4",
+        tashqi_xotira: "64GB",
+        narx: "3 002 000",
+        kafolat: "12",
+        version: "Android 11",
+      },
+    ],
+  };
+
+  render() {
+      const {product}=this.state;
+    return (
+      <div>
+         <Container> 
+          <div className='cards'>
+          {
+              product && Array.isArray(product) ? product.map(item=>{
+                return (
+                    <div>
+                        {/* <img src={foto} /> */}
+                       <h2>{item.name}</h2>
+                    </div>
+
+                )
+              }):""
+          }
+        </div>
+        </Container>
+      </div>
+    );
+  }
 }
