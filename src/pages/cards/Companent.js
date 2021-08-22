12c1902a7731bbf6a3c4ad17f5d43293606bbc05
Bahriddin0105 <87873365+Bahriddin0105@users.noreply.github.com>
2021-08-22 22:29:06 +0500

    /

--- src/pages/cards/Companent.js
+++ src/pages/cards/Companent.js
@@ -1,10 +1,36 @@
 import React, { Component } from "react";
-import { Container } from "react-bootstrap";
-import foto from '../../images/foto.jpeg'
-import './Comp.css'
+import foto from "../../images/foto.jpeg";
+import "./Comp.css";
 export default class Companent extends Component {
   state = {
     product: [
+      {
+        id: 1,
+        name: "Samsung Galaxy A32 4/64GB",
+        ichki_xotira: "4",
+        tashqi_xotira: "64GB",
+        narx: "3 002 000",
+        kafolat: "12",
+        version: "Android 11",
+      },
+      {
+        id: 1,
+        name: "Samsung Galaxy A32 4/64GB",
+        ichki_xotira: "4",
+        tashqi_xotira: "64GB",
+        narx: "3 002 000",
+        kafolat: "12",
+        version: "Android 11",
+      },
+      {
+        id: 1,
+        name: "Samsung Galaxy A32 4/64GB",
+        ichki_xotira: "4",
+        tashqi_xotira: "64GB",
+        narx: "3 002 000",
+        kafolat: "12",
+        version: "Android 11",
+      },
       {
         id: 1,
         name: "Samsung Galaxy A32 4/64GB",
@@ -54,26 +80,33 @@ export default class Companent extends Component {
   };
 
   render() {
-      const {product}=this.state;
+    const { product } = this.state;
     return (
       <div>
-         <div className='bigSheet'> 
-         <div className='first'>
-           Assalomu aleykum
-         </div>
-          <div className='second'>
-          {
-              product && Array.isArray(product) ? product.map(item=>{
-                return (
-                    <div className='cards'>
-                        <img src={foto} className='foto' />
-                       <h2>{item.name}</h2>
-                    </div>
+        <div className="bigSheet">
+          <div className="first">
+            <div className='hi'>
+              <h1>Рассрочка</h1>
+              <p>Без оформления кредита</p>
+              <img src={foto} className='foto' />
+              <button className='btn'>Просмотреть все</button>
 
-                )
-              }):""
-          }
-        </div>
+              
+            </div>
+          </div>
+          <div className="second">
+            {product && Array.isArray(product)
+              ? product.map((item) => {
+                  return (
+                    <div className="cards">
+                      <img src={foto} className="foto" />
+                      <p>{item.name}</p>
+                      <h3>{item.narx} сум</h3>
+                    </div>
+                  );
+                })
+              : ""}
+          </div>
         </div>
       </div>
     );
